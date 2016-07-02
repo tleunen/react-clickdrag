27784ef50798b319e781419aa0f36ddb97f43806
Tommy Leunen <tommy.leunen+github@gmail.com>
2016-07-02 19:17:34 -0400

    test(scripts): Fix tests with jsdom 9.4.0

--- test/setup.js
+++ test/setup.js
@@ -1,16 +1,7 @@
 const jsdom = require('jsdom').jsdom;
 
-const exposedProperties = ['window', 'navigator', 'document'];
-
-global.document = jsdom('');
+global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-    if (typeof global[property] === 'undefined') {
-        exposedProperties.push(property);
-        global[property] = document.defaultView[property];
-    }
-});
+global.navigator = window.navigator;
 
-global.navigator = {
-    userAgent: 'node.js'
-};
+global.MouseEvent = window.MouseEvent;
