74adbd11f59ae38fc6c7c9a888f21528219b0015
Tommy <tleunen@users.noreply.github.com>
2018-09-05 22:25:07 -0400

    feat: Update project to react16, jest, enzyme3 (#103)
    BREAKING CHANGE: This version is only compatible with React >= 16

--- src/clickdrag.jsx
+++ src/clickdrag.jsx
@@ -4,122 +4,122 @@ import { findDOMNode } from 'react-dom';
 const noop = () => {};
 
 function clickDrag(Component, opts = {}) {
-    const touch = opts.touch || false;
-    const resetOnSpecialKeys = opts.resetOnSpecialKeys || false;
-    const getSpecificEventData = opts.getSpecificEventData || (() => ({}));
+  const touch = opts.touch || false;
+  const resetOnSpecialKeys = opts.resetOnSpecialKeys || false;
+  const getSpecificEventData = opts.getSpecificEventData || (() => ({}));
 
-    const onDragStart = opts.onDragStart || noop;
-    const onDragStop = opts.onDragStop || noop;
-    const onDragMove = opts.onDragMove || noop;
+  const onDragStart = opts.onDragStart || noop;
+  const onDragStop = opts.onDragStop || noop;
+  const onDragMove = opts.onDragMove || noop;
 
-    class ClickDrag extends React.Component {
-        constructor(props) {
-            super(props);
+  class ClickDrag extends React.Component {
+    constructor(props) {
+      super(props);
 
-            this.onMouseDown = this.onMouseDown.bind(this);
-            this.onMouseUp = this.onMouseUp.bind(this);
-            this.onMouseMove = this.onMouseMove.bind(this);
+      this.onMouseDown = this.onMouseDown.bind(this);
+      this.onMouseUp = this.onMouseUp.bind(this);
+      this.onMouseMove = this.onMouseMove.bind(this);
 
-            this.state = {
-                isMouseDown: false,
-                isMoving: false,
-                mouseDownPositionX: 0,
-                mouseDownPositionY: 0,
-                moveDeltaX: 0,
-                moveDeltaY: 0,
-            };
+      this.state = {
+        isMouseDown: false,
+        isMoving: false,
+        mouseDownPositionX: 0,
+        mouseDownPositionY: 0,
+        moveDeltaX: 0,
+        moveDeltaY: 0,
+      };
 
-            this.wasUsingSpecialKeys = false;
+      this.wasUsingSpecialKeys = false;
+    }
+
+    componentDidMount() {
+      const node = findDOMNode(this);
+
+      node && node.addEventListener('mousedown', this.onMouseDown);
+      document.addEventListener('mousemove', this.onMouseMove);
+      document.addEventListener('mouseup', this.onMouseUp);
+
+      if (touch) {
+        node && node.addEventListener('touchstart', this.onMouseDown, { passive: true });
+        document.addEventListener('touchmove', this.onMouseMove);
+        document.addEventListener('touchend', this.onMouseUp);
+      }
+    }
+
+    componentWillUnmount() {
+      const node = findDOMNode(this);
+
+      node && node.removeEventListener('mousedown', this.onMouseDown);
+      document.removeEventListener('mousemove', this.onMouseMove);
+      document.removeEventListener('mouseup', this.onMouseUp);
+
+      if (touch) {
+        node && node.removeEventListener('touchstart', this.onMouseDown, { passive: true });
+        document.removeEventListener('touchmove', this.onMouseMove);
+        document.removeEventListener('touchend', this.onMouseUp);
+      }
+    }
+
+    onMouseDown(e) {
+      // only left mouse button
+      if (touch || e.button === 0) {
+        const pt = (e.changedTouches && e.changedTouches[0]) || e;
+
+        this.setMousePosition(pt.clientX, pt.clientY);
+
+        onDragStart(e);
+      }
+    }
+
+    onMouseUp(e) {
+      if (this.state.isMouseDown) {
+        this.setState({
+          isMouseDown: false,
+          isMoving: false,
+        });
+
+        onDragStop(e);
+      }
+    }
+
+    onMouseMove(e) {
+      if (this.state.isMouseDown) {
+        const pt = (e.changedTouches && e.changedTouches[0]) || e;
+
+        const isUsingSpecialKeys = e.metaKey || e.ctrlKey || e.shiftKey || e.altKey;
+        if (resetOnSpecialKeys && this.wasUsingSpecialKeys !== isUsingSpecialKeys) {
+          this.wasUsingSpecialKeys = isUsingSpecialKeys;
+          this.setMousePosition(pt.clientX, pt.clientY);
+        } else {
+          this.setState({
+            isMoving: true,
+            moveDeltaX: pt.clientX - this.state.mouseDownPositionX,
+            moveDeltaY: pt.clientY - this.state.mouseDownPositionY,
+            ...getSpecificEventData(e),
+          });
         }
 
-        componentDidMount() {
-            const node = findDOMNode(this);
+        onDragMove(e);
+      }
+    }
 
-            node.addEventListener('mousedown', this.onMouseDown);
-            document.addEventListener('mousemove', this.onMouseMove);
-            document.addEventListener('mouseup', this.onMouseUp);
+    setMousePosition(x, y) {
+      this.setState({
+        isMouseDown: true,
+        isMoving: false,
+        mouseDownPositionX: x,
+        mouseDownPositionY: y,
+        moveDeltaX: 0,
+        moveDeltaY: 0,
+      });
+    }
 
-            if (touch) {
-                node.addEventListener('touchstart', this.onMouseDown, {passive:true});
-                document.addEventListener('touchmove', this.onMouseMove);
-                document.addEventListener('touchend', this.onMouseUp);
-            }
-        }
-
-        componentWillUnmount() {
-            const node = findDOMNode(this);
-
-            node.removeEventListener('mousedown', this.onMouseDown);
-            document.removeEventListener('mousemove', this.onMouseMove);
-            document.removeEventListener('mouseup', this.onMouseUp);
-
-            if (touch) {
-                node.removeEventListener('touchstart', this.onMouseDown, {passive:true});
-                document.removeEventListener('touchmove', this.onMouseMove);
-                document.removeEventListener('touchend', this.onMouseUp);
-            }
-        }
-
-        onMouseDown(e) {
-            // only left mouse button
-            if (touch || e.button === 0) {
-                const pt = (e.changedTouches && e.changedTouches[0]) || e;
-
-                this.setMousePosition(pt.clientX, pt.clientY);
-
-                onDragStart(e);
-            }
-        }
-
-        onMouseUp(e) {
-            if (this.state.isMouseDown) {
-                this.setState({
-                    isMouseDown: false,
-                    isMoving: false,
-                });
-
-                onDragStop(e);
-            }
-        }
-
-        onMouseMove(e) {
-            if (this.state.isMouseDown) {
-                const pt = (e.changedTouches && e.changedTouches[0]) || e;
-
-                const isUsingSpecialKeys = e.metaKey || e.ctrlKey || e.shiftKey || e.altKey;
-                if (resetOnSpecialKeys && this.wasUsingSpecialKeys !== isUsingSpecialKeys) {
-                    this.wasUsingSpecialKeys = isUsingSpecialKeys;
-                    this.setMousePosition(pt.clientX, pt.clientY);
-                } else {
-                    this.setState({
-                        isMoving: true,
-                        moveDeltaX: pt.clientX - this.state.mouseDownPositionX,
-                        moveDeltaY: pt.clientY - this.state.mouseDownPositionY,
-                        ...getSpecificEventData(e),
-                    });
-                }
-
-                onDragMove(e);
-            }
-        }
-
-        setMousePosition(x, y) {
-            this.setState({
-                isMouseDown: true,
-                isMoving: false,
-                mouseDownPositionX: x,
-                mouseDownPositionY: y,
-                moveDeltaX: 0,
-                moveDeltaY: 0,
-            });
-        }
-
-        render() {
-            return <Component {...this.props} dataDrag={this.state} />;
-        }
+    render() {
+      return <Component {...this.props} dataDrag={this.state} />;
+    }
   }
 
-    return ClickDrag;
+  return ClickDrag;
 }
 
 export default clickDrag;
--- test/clickdrag.test.jsx
+++ test/clickdrag.test.jsx
@@ -1,316 +1,315 @@
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-/* eslint-env mocha */
-
 import React from 'react';
 import { findDOMNode } from 'react-dom';
-import { expect } from 'chai';
-import sinon from 'sinon';
 import { mount } from 'enzyme';
 import clickdrag from '../src/clickdrag';
 
 const initialState = {
-    isMouseDown: false,
-    isMoving: false,
-    mouseDownPositionX: 0,
-    mouseDownPositionY: 0,
-    moveDeltaX: 0,
-    moveDeltaY: 0,
+  isMouseDown: false,
+  isMoving: false,
+  mouseDownPositionX: 0,
+  mouseDownPositionY: 0,
+  moveDeltaX: 0,
+  moveDeltaY: 0,
 };
 
 function mouseDownAt(node, x, y, rightClick) {
-    node.dispatchEvent(new MouseEvent('mousedown', {
-        button: rightClick ? 1 : 0,
-        clientX: x,
-        clientY: y,
-    }));
+  node.dispatchEvent(
+    new MouseEvent('mousedown', {
+      button: rightClick ? 1 : 0,
+      clientX: x,
+      clientY: y,
+    })
+  );
 }
 
 function mouseMove(x, y, ctrl, shift, meta) {
-    document.dispatchEvent(new MouseEvent('mousemove', {
-        clientX: x,
-        clientY: y,
-        shiftKey: !!shift,
-        metaKey: !!meta,
-        ctrlKey: !!ctrl,
-    }));
+  document.dispatchEvent(
+    new MouseEvent('mousemove', {
+      clientX: x,
+      clientY: y,
+      shiftKey: !!shift,
+      metaKey: !!meta,
+      ctrlKey: !!ctrl,
+    })
+  );
 }
 
 it('should render the given component', () => {
-    const Comp = () => <div>Hello</div>;
-    const DecoraredComp = clickdrag(Comp);
-    const wrapper = mount(<DecoraredComp />);
+  const Comp = () => <div>Hello</div>;
+  const DecoraredComp = clickdrag(Comp);
+  const wrapper = mount(<DecoraredComp />);
 
-    expect(wrapper.html()).to.equal('<div>Hello</div>');
+  expect(wrapper.html()).toEqual('<div>Hello</div>');
 });
 
 it('should have a default state on first render', () => {
+  const Comp = () => <div>Hello</div>;
+  const DecoraredComp = clickdrag(Comp);
+  const wrapper = mount(<DecoraredComp />);
+
+  expect(wrapper.state()).toEqual(initialState);
+});
+
+describe('attach/detach events', () => {
+  let spyAttach;
+  let spyDetach;
+
+  beforeEach(() => {
+    spyAttach = jest.spyOn(document, 'addEventListener').mockImplementation(() => jest.fn());
+    spyDetach = jest.spyOn(document, 'removeEventListener').mockImplementation(() => jest.fn());
+  });
+
+  afterEach(() => {
+    spyAttach.mockRestore();
+    spyDetach.mockRestore();
+  });
+
+  it('should attach events on document', () => {
+    const Comp = () => <div>Hello</div>;
+    const DecoraredComp = clickdrag(Comp);
+
+    mount(<DecoraredComp />);
+
+    expect(spyAttach).toHaveBeenCalledTimes(2);
+    expect(spyAttach.mock.calls[0][0]).toBe('mousemove');
+    expect(spyAttach.mock.calls[1][0]).toBe('mouseup');
+  });
+
+  it('should detach events on document', () => {
+    const Comp = () => <div>Hello</div>;
+    const DecoraredComp = clickdrag(Comp);
+
+    const wrapper = mount(<DecoraredComp />);
+
+    wrapper.unmount();
+
+    expect(spyDetach).toHaveBeenCalledTimes(2);
+    expect(spyDetach.mock.calls[0][0]).toBe('mousemove');
+    expect(spyDetach.mock.calls[1][0]).toBe('mouseup');
+  });
+});
+
+describe('mousedown', () => {
+  it('should set the mouse position and isMouseDown on left mousedown', () => {
     const Comp = () => <div>Hello</div>;
     const DecoraredComp = clickdrag(Comp);
     const wrapper = mount(<DecoraredComp />);
 
-    expect(wrapper.state()).to.eql(initialState);
-});
+    // because we manually listen on the node, we cannot use enzyme simulate
+    mouseDownAt(findDOMNode(wrapper.instance()), 132, 642);
 
-describe('attach/detach events', () => {
-    const spyAttach = sinon.spy();
-    const spyDetach = sinon.spy();
-    let stubAttach;
-    let stubDetach;
-
-    beforeEach(() => {
-        stubAttach = sinon.stub(document, 'addEventListener', spyAttach);
-        stubDetach = sinon.stub(document, 'removeEventListener', spyDetach);
+    expect(wrapper.state()).toEqual({
+      ...initialState,
+      isMouseDown: true,
+      mouseDownPositionX: 132,
+      mouseDownPositionY: 642,
     });
+  });
 
-    afterEach(() => {
-        stubAttach.restore();
-        stubDetach.restore();
-    });
+  it('should not set the mouse position on right mousedown', () => {
+    const Comp = () => <div>Hello</div>;
+    const DecoraredComp = clickdrag(Comp);
+    const wrapper = mount(<DecoraredComp />);
 
-    it('should attach events on document', () => {
-        const Comp = () => <div>Hello</div>;
-        const DecoraredComp = clickdrag(Comp);
+    // because we manually listen on the node, we cannot use enzyme simulate
+    mouseDownAt(findDOMNode(wrapper.instance()), 1, 6, true);
 
-        mount(<DecoraredComp />);
-
-        expect(spyAttach.calledWith('mouseup')).to.equal(true);
-        expect(spyAttach.calledWith('mousemove')).to.equal(true);
-    });
-
-    it('should detach events on document', () => {
-        const Comp = () => <div>Hello</div>;
-        const DecoraredComp = clickdrag(Comp);
-
-        const wrapper = mount(<DecoraredComp />);
-
-        wrapper.unmount();
-
-        expect(spyDetach.calledWith('mouseup')).to.equal(true);
-        expect(spyDetach.calledWith('mousemove')).to.equal(true);
-    });
-});
-
-describe('mousedown', () => {
-    it('should set the mouse position and isMouseDown on left mousedown', () => {
-        const Comp = () => <div>Hello</div>;
-        const DecoraredComp = clickdrag(Comp);
-        const wrapper = mount(<DecoraredComp />);
-
-        // because we manually listen on the node, we cannot use enzyme simulate
-        mouseDownAt(findDOMNode(wrapper.node), 132, 642);
-
-        expect(wrapper.state()).to.eql({
-            ...initialState,
-            isMouseDown: true,
-            mouseDownPositionX: 132,
-            mouseDownPositionY: 642,
-        });
-    });
-
-    it('should not set the mouse position on right mousedown', () => {
-        const Comp = () => <div>Hello</div>;
-        const DecoraredComp = clickdrag(Comp);
-        const wrapper = mount(<DecoraredComp />);
-
-        // because we manually listen on the node, we cannot use enzyme simulate
-        mouseDownAt(findDOMNode(wrapper.node), 1, 6, true);
-
-        expect(wrapper.state()).to.eql(initialState);
-    });
+    expect(wrapper.state()).toEqual(initialState);
+  });
 });
 
 describe('mouseup', () => {
-    it('should not update the sate without previously down', () => {
-        const Comp = () => <div>Hello</div>;
-        const DecoraredComp = clickdrag(Comp);
-        const wrapper = mount(<DecoraredComp />);
+  it('should not update the sate without previously down', () => {
+    const Comp = () => <div>Hello</div>;
+    const DecoraredComp = clickdrag(Comp);
+    const wrapper = mount(<DecoraredComp />);
 
-        document.dispatchEvent(new MouseEvent('mouseup'));
+    document.dispatchEvent(new MouseEvent('mouseup'));
 
-        expect(wrapper.state()).to.eql(initialState);
+    expect(wrapper.state()).toEqual(initialState);
+  });
+
+  it('should reset isMouseDown', () => {
+    const Comp = () => <div>Hello</div>;
+    const DecoraredComp = clickdrag(Comp);
+    const wrapper = mount(<DecoraredComp />);
+
+    mouseDownAt(findDOMNode(wrapper.instance()), 132, 642);
+    document.dispatchEvent(new MouseEvent('mouseup'));
+
+    expect(wrapper.state()).toEqual({
+      ...initialState,
+      isMouseDown: false,
+      mouseDownPositionX: 132,
+      mouseDownPositionY: 642,
     });
-
-    it('should reset isMouseDown', () => {
-        const Comp = () => <div>Hello</div>;
-        const DecoraredComp = clickdrag(Comp);
-        const wrapper = mount(<DecoraredComp />);
-
-        mouseDownAt(findDOMNode(wrapper.node), 132, 642);
-        document.dispatchEvent(new MouseEvent('mouseup'));
-
-        expect(wrapper.state()).to.eql({
-            ...initialState,
-            isMouseDown: false,
-            mouseDownPositionX: 132,
-            mouseDownPositionY: 642,
-        });
-    });
+  });
 });
 
 describe('mousemove', () => {
-    it('should not update the sate without previously down', () => {
-        const Comp = () => <div>Hello</div>;
-        const DecoraredComp = clickdrag(Comp);
-        const wrapper = mount(<DecoraredComp />);
+  it('should not update the sate without previously down', () => {
+    const Comp = () => <div>Hello</div>;
+    const DecoraredComp = clickdrag(Comp);
+    const wrapper = mount(<DecoraredComp />);
 
-        document.dispatchEvent(new MouseEvent('mousemove'));
+    document.dispatchEvent(new MouseEvent('mousemove'));
 
-        expect(wrapper.state()).to.eql(initialState);
+    expect(wrapper.state()).toEqual(initialState);
+  });
+
+  it('should set the move delta values', () => {
+    const Comp = () => <div>Hello</div>;
+    const DecoraredComp = clickdrag(Comp);
+    const wrapper = mount(<DecoraredComp />);
+
+    mouseDownAt(findDOMNode(wrapper.instance()), 100, 150);
+    mouseMove(50, 125);
+
+    expect(wrapper.state()).toEqual({
+      isMouseDown: true,
+      isMoving: true,
+      mouseDownPositionX: 100,
+      mouseDownPositionY: 150,
+      moveDeltaX: -50,
+      moveDeltaY: -25,
     });
+  });
 
-    it('should set the move delta values', () => {
-        const Comp = () => <div>Hello</div>;
-        const DecoraredComp = clickdrag(Comp);
-        const wrapper = mount(<DecoraredComp />);
-
-        mouseDownAt(findDOMNode(wrapper.node), 100, 150);
-        mouseMove(50, 125);
-
-        expect(wrapper.state()).to.eql({
-            isMouseDown: true,
-            isMoving: true,
-            mouseDownPositionX: 100,
-            mouseDownPositionY: 150,
-            moveDeltaX: -50,
-            moveDeltaY: -25,
-        });
+  it('should also set the specified custom event data', () => {
+    const Comp = () => <div>Hello</div>;
+    const DecoraredComp = clickdrag(Comp, {
+      getSpecificEventData: e => ({
+        shiftKey: e.shiftKey,
+        ctrlKey: e.ctrlKey,
+      }),
     });
+    const wrapper = mount(<DecoraredComp />);
 
-    it('should also set the specified custom event data', () => {
-        const Comp = () => <div>Hello</div>;
-        const DecoraredComp = clickdrag(Comp, {
-            getSpecificEventData: (e) => ({
-                shiftKey: e.shiftKey,
-                ctrlKey: e.ctrlKey,
-            }),
-        });
-        const wrapper = mount(<DecoraredComp />);
+    mouseDownAt(findDOMNode(wrapper.instance()), 100, 150);
+    mouseMove(50, 125, false, true);
 
-        mouseDownAt(findDOMNode(wrapper.node), 100, 150);
-        mouseMove(50, 125, false, true);
-
-        expect(wrapper.state()).to.eql({
-            isMouseDown: true,
-            isMoving: true,
-            mouseDownPositionX: 100,
-            mouseDownPositionY: 150,
-            moveDeltaX: -50,
-            moveDeltaY: -25,
-            shiftKey: true,
-            ctrlKey: false,
-        });
+    expect(wrapper.state()).toEqual({
+      isMouseDown: true,
+      isMoving: true,
+      mouseDownPositionX: 100,
+      mouseDownPositionY: 150,
+      moveDeltaX: -50,
+      moveDeltaY: -25,
+      shiftKey: true,
+      ctrlKey: false,
     });
+  });
 
-    it('should not reset the mouse position when clicking on a special key', () => {
-        const Comp = () => <div>Hello</div>;
-        const DecoraredComp = clickdrag(Comp, {
-            getSpecificEventData: (e) => ({
-                ctrlKey: e.ctrlKey,
-            }),
-        });
-        const wrapper = mount(<DecoraredComp />);
-
-        mouseDownAt(findDOMNode(wrapper.node), 100, 150);
-        mouseMove(50, 125);
-        mouseMove(1, 173, true);
-
-        expect(wrapper.state()).to.eql({
-            isMouseDown: true,
-            isMoving: true,
-            mouseDownPositionX: 100,
-            mouseDownPositionY: 150,
-            moveDeltaX: -99,
-            moveDeltaY: 23,
-            ctrlKey: true,
-        });
+  it('should not reset the mouse position when clicking on a special key', () => {
+    const Comp = () => <div>Hello</div>;
+    const DecoraredComp = clickdrag(Comp, {
+      getSpecificEventData: e => ({
+        ctrlKey: e.ctrlKey,
+      }),
     });
+    const wrapper = mount(<DecoraredComp />);
 
-    it('should reset the mouse position when clicking on a special key, with resetOnSpecialKeys', () => {
-        const Comp = () => <div>Hello</div>;
-        const DecoraredComp = clickdrag(Comp, {
-            resetOnSpecialKeys: true,
-            getSpecificEventData: (e) => ({
-                ctrlKey: e.ctrlKey,
-            }),
-        });
-        const wrapper = mount(<DecoraredComp />);
+    mouseDownAt(findDOMNode(wrapper.instance()), 100, 150);
+    mouseMove(50, 125);
+    mouseMove(1, 173, true);
 
-        mouseDownAt(findDOMNode(wrapper.node), 100, 150);
-        mouseMove(50, 125);
-        mouseMove(1, 173, true);
-        mouseMove(1, 174, true);
-
-        expect(wrapper.state()).to.eql({
-            isMouseDown: true,
-            isMoving: true,
-            mouseDownPositionX: 1,
-            mouseDownPositionY: 173,
-            moveDeltaX: 0,
-            moveDeltaY: 1,
-            ctrlKey: true,
-        });
+    expect(wrapper.state()).toEqual({
+      isMouseDown: true,
+      isMoving: true,
+      mouseDownPositionX: 100,
+      mouseDownPositionY: 150,
+      moveDeltaX: -99,
+      moveDeltaY: 23,
+      ctrlKey: true,
     });
+  });
+
+  it('should reset the mouse position when clicking on a special key, with resetOnSpecialKeys', () => {
+    const Comp = () => <div>Hello</div>;
+    const DecoraredComp = clickdrag(Comp, {
+      resetOnSpecialKeys: true,
+      getSpecificEventData: e => ({
+        ctrlKey: e.ctrlKey,
+      }),
+    });
+    const wrapper = mount(<DecoraredComp />);
+
+    mouseDownAt(findDOMNode(wrapper.instance()), 100, 150);
+    mouseMove(50, 125);
+    mouseMove(1, 173, true);
+    mouseMove(1, 174, true);
+
+    expect(wrapper.state()).toEqual({
+      isMouseDown: true,
+      isMoving: true,
+      mouseDownPositionX: 1,
+      mouseDownPositionY: 173,
+      moveDeltaX: 0,
+      moveDeltaY: 1,
+      ctrlKey: true,
+    });
+  });
 });
 
 describe('handle touch events', () => {
-    describe('attach/detach events', () => {
-        const spyAttach = sinon.spy();
-        const spyDetach = sinon.spy();
-        let stubAttach;
-        let stubDetach;
+  describe('attach/detach events', () => {
+    let spyAttach;
+    let spyDetach;
 
-        beforeEach(() => {
-            stubAttach = sinon.stub(document, 'addEventListener', spyAttach);
-            stubDetach = sinon.stub(document, 'removeEventListener', spyDetach);
-        });
-
-        afterEach(() => {
-            stubAttach.restore();
-            stubDetach.restore();
-        });
-
-        it('should attach events on document', () => {
-            const Comp = () => <div>Hello</div>;
-            const DecoraredComp = clickdrag(Comp, { touch: true });
-
-            mount(<DecoraredComp />);
-
-            expect(spyAttach.calledWith('touchmove')).to.equal(true);
-            expect(spyAttach.calledWith('touchend')).to.equal(true);
-        });
-
-        it('should detach events on document', () => {
-            const Comp = () => <div>Hello</div>;
-            const DecoraredComp = clickdrag(Comp, { touch: true });
-
-            const wrapper = mount(<DecoraredComp />);
-
-            wrapper.unmount();
-
-            expect(spyDetach.calledWith('touchmove')).to.equal(true);
-            expect(spyDetach.calledWith('touchend')).to.equal(true);
-        });
+    beforeEach(() => {
+      spyAttach = jest.spyOn(document, 'addEventListener').mockImplementation(() => jest.fn());
+      spyDetach = jest.spyOn(document, 'removeEventListener').mockImplementation(() => jest.fn());
     });
 
-    // describe('touchstart', () => {
-    //     it('should set the mouse position and isMouseDown', () => {
-    //         const Comp = () => <div>Hello</div>;
-    //         const DecoraredComp = clickdrag(Comp, { touch: true });
-    //         const wrapper = mount(<DecoraredComp />);
-    //
-    //         findDOMNode(wrapper.node).dispatchEvent(new MouseEvent('touchstart', {
-    //             changedTouches: [{
-    //                 clientX: 1,
-    //                 clientY: 4
-    //             }]
-    //         }));
-    //
-    //         expect(wrapper.state()).to.eql({
-    //             ...initialState,
-    //             isMouseDown: true,
-    //             mouseDownPositionX: 123,
-    //             mouseDownPositionY: 321
-    //         });
-    //     });
-    // });
+    afterEach(() => {
+      spyAttach.mockRestore();
+      spyDetach.mockRestore();
+    });
+
+    it('should attach events on document', () => {
+      const Comp = () => <div>Hello</div>;
+      const DecoraredComp = clickdrag(Comp, { touch: true });
+
+      mount(<DecoraredComp />);
+
+      expect(spyAttach).toHaveBeenCalledTimes(4);
+      expect(spyAttach.mock.calls[2][0]).toBe('touchmove');
+      expect(spyAttach.mock.calls[3][0]).toBe('touchend');
+    });
+
+    it('should detach events on document', () => {
+      const Comp = () => <div>Hello</div>;
+      const DecoraredComp = clickdrag(Comp, { touch: true });
+
+      const wrapper = mount(<DecoraredComp />);
+
+      wrapper.unmount();
+
+      expect(spyDetach).toHaveBeenCalledTimes(4);
+      expect(spyDetach.mock.calls[2][0]).toBe('touchmove');
+      expect(spyDetach.mock.calls[3][0]).toBe('touchend');
+    });
+  });
+
+  // describe('touchstart', () => {
+  //     it('should set the mouse position and isMouseDown', () => {
+  //         const Comp = () => <div>Hello</div>;
+  //         const DecoraredComp = clickdrag(Comp, { touch: true });
+  //         const wrapper = mount(<DecoraredComp />);
+  //
+  //         findDOMNode(wrapper.instance()).dispatchEvent(new MouseEvent('touchstart', {
+  //             changedTouches: [{
+  //                 clientX: 1,
+  //                 clientY: 4
+  //             }]
+  //         }));
+  //
+  //         expect(wrapper.state()).toBe({
+  //             ...initialState,
+  //             isMouseDown: true,
+  //             mouseDownPositionX: 123,
+  //             mouseDownPositionY: 321
+  //         });
+  //     });
+  // });
 });
